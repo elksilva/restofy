d0b22ddd11be6eac3388a4f145d3e20ca199ee2a
jose silva <elksilva@hotmail.com>
2024-02-24 21:22:39 -0500

    primer commit subir a github

---
+++ assets/js/utilidad.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener todos los botones de "ver más"
+    const botonesVerMas = document.querySelectorAll(".ver-mas");
+
+    // Iterar sobre cada botón de "ver más"
+    botonesVerMas.forEach(function(boton) {
+      // Configurar evento de clic para mostrar el contenido completo
+      boton.addEventListener("click", function() {
+        const parrafo = boton.previousElementSibling; // Obtener el párrafo hermano anterior
+        parrafo.classList.toggle("show-more"); // Alternar la clase show-more para colapsar o expandir el párrafo
+        if (parrafo.classList.contains("show-more")) {
+          boton.textContent = "Ver menos"; // Cambiar el texto del botón a "Ver menos" cuando se expanda el párrafo
+        } else {
+          boton.textContent = "Ver más"; // Cambiar el texto del botón a "Ver más" cuando se colapse el párrafo
+        }
+      });
+    });
+  });
+  

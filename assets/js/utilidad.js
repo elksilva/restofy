08b666e72846a0bf82d1be09d3b32b4e34a4e753
Julian Rodriguez <julianlink123@hotmail.com>
2024-03-03 22:00:12 -0500

    pagina precios añadida

--- assets/js/utilidad.js
+++ assets/js/utilidad.js
@@ -2,16 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // Obtener todos los botones de "ver más"
     const botonesVerMas = document.querySelectorAll(".ver-mas");
 
-    // Iterar sobre cada botón de "ver más"
     botonesVerMas.forEach(function(boton) {
-      // Configurar evento de clic para mostrar el contenido completo
       boton.addEventListener("click", function() {
         const parrafo = boton.previousElementSibling; // Obtener el párrafo hermano anterior
-        parrafo.classList.toggle("show-more"); // Alternar la clase show-more para colapsar o expandir el párrafo
+        parrafo.classList.toggle("show-more"); 
         if (parrafo.classList.contains("show-more")) {
-          boton.textContent = "Ver menos"; // Cambiar el texto del botón a "Ver menos" cuando se expanda el párrafo
+          boton.textContent = "Ver menos"; 
         } else {
-          boton.textContent = "Ver más"; // Cambiar el texto del botón a "Ver más" cuando se colapse el párrafo
+          boton.textContent = "Ver más"; 
         }
       });
     });
